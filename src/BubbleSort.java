78651722b6b35549ab9aed3cd438857535fc890d
AmitPatelup65 <patelboy330@gmail.com>
2025-10-05 19:47:39 +0530

    Sorting

---
+++ src/BubbleSort.java
@@ -0,0 +1,22 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int  []arr={5,2,1,3,4};
+        int n =arr.length;
+        //Optimized Way Of Bubble sorting
+        for(int i=0;i<n;i++){
+            boolean check=true;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    check=false;
+                }
+            }
+            if(check==true) break;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
--- src/Factorial.java
+++ src/Factorial.java
@@ -1,4 +1,11 @@
 public class Factorial {
-    int a=5;
+    public static void main(String[] args) {
+        int a = 4;
+        int fact = 1;
+        for (int i = a; i >= 1; i--) {
+           fact=fact*i;
 
+        }
+        System.out.print(fact);
+    }
 }
