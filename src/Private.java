00117d9c39e84a65831acddd4ebf296e628f33b1
AmitPatelup65 <patelboy330@gmail.com>
2025-10-06 22:08:55 +0530

    update

---
+++ src/Private.java
@@ -0,0 +1,16 @@
+class stu{
+    int age;
+    private int roll;
+    void print (){
+        System.out.println(roll);
+    }
+}
+public class Private {
+
+    public static void main(String[] args) {
+        stu stu = new stu();
+        stu.print();
+
+
+    }
+}
