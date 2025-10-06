7fb42ef12c89b4a5f5edb63e5bb00778d4c809e1
AmitPatelup65 <patelboy330@gmail.com>
2025-10-06 22:33:47 +0530

    update

--- src/Constructor.java
+++ src/Constructor.java
@@ -2,12 +2,14 @@ public class Constructor {
     public static class cons{
         int age;
         int rol;
-        cons(int age,int b){
-            System.out.println(age);
-            System.out.println(b);
+        cons(int a,int b){
+            age=a;
+            rol=b;
         }
     }
     public static void main(String[] args) {
         cons cons = new cons(23,22);
+        System.out.println(cons.age);
+        System.out.println(cons.rol);
     }
 }
